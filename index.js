6bfe8b2eb4129c9c30e1e69a93dcef3e80e9e9dc
ThanhPhat2611 <phat_dpm215509@student.agu.edu.vn>
2025-05-23 09:47:00 +0700

    Update cho t

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use(session({
 	name: 'iNews',						// Tên session (tự chọn)
